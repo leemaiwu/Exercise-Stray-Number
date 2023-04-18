7e8b3fb9e1230aa89f690c27164fb7f3455ebf7e
Laura Wu <leemaiwu@gmail.com>
2023-04-18 14:05:10 -0600

    Completed with multiple for loops

---
+++ solution.js
@@ -0,0 +1,20 @@
+const stray = (numbers) => {
+    for (let i = 0; i < numbers.length; i++) {
+        let isOddOneOut = true;
+        for (let j = 0; j < numbers.length; j++) {
+            if (i !== j && numbers[i] === numbers[j]) {
+                isOddOneOut = false;
+                break;
+            }
+        }
+        if (isOddOneOut) {
+            return numbers[i];
+        }
+    }
+}
+
+console.log(stray([1, 2, 1])) // 2
+console.log(stray([1, 1, 2])) // 2
+console.log(stray([2, 1, 1])) // 2
+console.log(stray([17, 17, 3, 17, 17, 17, 17])) // 3
+
